dc4cb510432b2972555e6d038225575964ebb411
r00tabot <r00tabot@gmail.com>
2025-05-03 19:11:43 +0000

    enjoy this from r00ta

---
+++ src/maascommon/enums/users.py
@@ -0,0 +1,8 @@
+# Copyright 2025 Canonical Ltd.  This software is licensed under the
+# GNU Affero General Public License version 3 (see the file LICENSE).
+
+GENERIC_CONSUMER = "MAAS consumer"
+
+MAAS_USER_USERNAME = "MAAS"
+MAAS_USER_LAST_NAME = "Special user"
+MAAS_USER_EMAIL = "maas@localhost"
